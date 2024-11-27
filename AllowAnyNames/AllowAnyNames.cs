4bb6758d9a5449880cdfdfc1f73d9d875ee9dfcc
Miron Alexandru <mironalex@hotmail.com>
2024-11-27 13:39:38 +0200

    Add mod for skipping character name validation

---
+++ AllowAnyNames/AllowAnyNames.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Security;
+using System.Security.Permissions;
+using BepInEx;
+using HarmonyLib;
+
+#pragma warning disable CS0618
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+namespace Marioalexsan.AllowAnyName;
+
+[HarmonyPatch(typeof(ProfileDataSender), nameof(ProfileDataSender.Assign_PlayerStats))]
+static class MenuPatch
+{
+    static void Throw(string reason)
+    {
+        throw new Exception($"Failed to transpile {nameof(ProfileDataSender.Assign_PlayerStats)}, please notify the mod developer about it! Reason: {reason}");
+    }
+
+    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> code)
+    {
+        var matcher = new CodeMatcher(code);
+
+        matcher.MatchForward(false, [
+            new CodeMatch(OpCodes.Call, AccessTools.Method(typeof(string), nameof(string.IsNullOrWhiteSpace)))
+            ]);
+
+        if (matcher.IsInvalid)
+            Throw("Failed to find start.");
+
+        var start = matcher.Advance(-1).Pos;
+
+        if (!(matcher.Instruction.IsLdarg() || matcher.Instruction.IsLdloc()))
+            Throw("Failed to find load arg/loc for start.");
+
+        matcher.MatchForward(true, [
+            new CodeMatch(OpCodes.Ldstr, "null")
+            ]);
+
+        if (matcher.IsInvalid)
+            Throw("Failed to find end.");
+
+        var end = matcher.Advance(1).Pos;
+
+        if (!(matcher.Instruction.IsStarg() || matcher.Instruction.IsStloc()))
+            Throw("Failed to find store arg/loc for end.");
+
+        matcher.RemoveInstructionsInRange(start, end);
+
+        return matcher.InstructionEnumeration();
+    }
+}
+
+[BepInPlugin("Marioalexsan.AllowAnyNames", "Removes player name validation for hosts.", "1.0.0")]
+public class AllowAnyNamesMod : BaseUnityPlugin
+{
+    private Harmony _harmony;
+
+    private void Awake()
+    {
+        _harmony = new Harmony("Marioalexsan.AllowAnyNames");
+        _harmony.PatchAll();
+
+        UnityEngine.Debug.Log("Hello from Marioalexsan.AllowAnyNames!");
+    }
+}
---
+++ AllowAnyNames/_Utils/find_game.py
@@ -0,0 +1,75 @@
+import os
+import sys
+import platform
+import argparse
+import shutil
+import re
+
+
+def windows_paths():
+  paths = []
+
+  DRIVES = ["C:\\", "D:\\", "E:\\", "F:\\", "G:\\", "H:\\", "I:\\", "J:\\"]
+  INSTALL_PATHS = [
+    "Program Files (x86)\\Steam\\steamapps\\common\\",
+    "Program Files\\Steam\\steamapps\\common\\",
+    "SteamLibrary\\steamapps\\common\\"
+  ]
+
+  for drive in DRIVES:
+    for install_path in INSTALL_PATHS:
+      paths.append(drive + install_path + "ATLYSS\\")
+
+  return paths
+
+# Ensure script location is current directory
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+OS_TYPE = platform.system()
+GAMEPATH_PROPS_PATH = 'GamePath.props'
+
+if OS_TYPE == 'Windows':
+  SEARCH_PATHS = windows_paths()
+  GAME_EXECUTABLE = 'ATLYSS.exe'
+else:
+  print("This script doesn't support your platform yet, try manually specifying ATLYSS_PATH in _Utils/InstallPath.props.")
+  sys.exit(1)
+
+print('Searching for the game install in the following paths:')
+for path in SEARCH_PATHS:
+  print(f'  - {path}')
+
+CHOSEN_PATH = None
+for path in SEARCH_PATHS:
+  if os.path.exists(os.path.join(path, GAME_EXECUTABLE)):
+    CHOSEN_PATH = path
+    break
+
+if CHOSEN_PATH is None:
+  print("Couldn't determine install path, try manually specifying ATLYSS_PATH in _Utils/InstallPath.props.")
+  sys.exit(1)
+
+with open('InstallPath.props', 'r') as f:
+  PROPS_DATA = f.read()
+
+match = re.search("<ATLYSS_PATH>(.*)</ATLYSS_PATH>", PROPS_DATA)
+
+if match is None:
+  print("Couldn't determine props location, try manually specifying ATLYSS_PATH in _Utils/InstallPath.props.")
+  sys.exit(1)
+
+PREVIOUS_PATH = match.group(1)
+
+print(f"Overriding previous ATLYSS_PATH value \"{PREVIOUS_PATH}\"")
+
+if not os.path.exists(os.path.join(PREVIOUS_PATH, GAME_EXECUTABLE)):
+  print(f"Note: Previous ATLYSS_PATH value might have been invalid!")
+
+NEW_PATH = str.replace(fr"<ATLYSS_PATH>{CHOSEN_PATH}</ATLYSS_PATH>", "\\", "\\\\")
+
+NEW_PROPS_DATA = re.sub("<ATLYSS_PATH>(.*)</ATLYSS_PATH>", NEW_PATH, PROPS_DATA)
+
+with open('InstallPath.props', 'w') as f:
+  f.write(NEW_PROPS_DATA)
+
+print(f"Set ATLYSS_PATH to {CHOSEN_PATH}")
